e3cb18c89ff9ab602042ed60006d00b8bde1785d
marcoBelt99 <marco01.beltrame@edu.unife.it>
2024-09-23 23:24:15 +0200

    aggiunti tutti i files da pushare

---
+++ VUE02/src/App.vue
@@ -0,0 +1,42 @@
+<script>
+  export default {
+    data() { // Funzione data()
+      return  { // ritorna un oggetto
+        nome : 'Marco',
+        stato: 'pending',
+        esamiMancanti: ['Ing. SW Avanz.', 
+          'Data Mining', 'Progr. Conc.',
+          'Ricerca Operativa'
+        ],
+        sitoGoogle: "https://google.com",
+      };
+    },
+    methods: { // Oggetto di metodi
+      cambiaStato() {
+        if(this.stato === 'active') {
+          this.status = 'pending';
+        }
+        else if( this.stato === 'pending') {
+          this.stato = 'inactive';
+        }
+        else {
+          this.stato = 'active';
+        }
+      }
+    }  
+  }
+</script>
+
+<template>
+  <h2 v-if="stato === 'active'">Utente e' attivo </h2>
+  <h2 v-else-if="stato === 'pending'">Utente e' in pending </h2>
+  <h2 v-else>Utente e' inattivo </h2>
+  <h3>Esami Mancanti:</h3>
+  <ul>
+    <li v-for="e in esamiMancanti" :key="esame"> {{ e }}</li>
+  </ul>
+ 
+  <a :href="sitoGoogle">Clicca per andare su Google</a>
+  <br>
+  <button v-on:click="cambiaStato">Cambia stato</button>
+</template>
